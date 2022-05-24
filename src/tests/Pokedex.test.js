616f0d48f11a4782525fd8d05ac0a1f8af142fc5
martins <gabrielamspinola@gmail.com>
2022-05-24 18:26:16 -0300

    Req 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -59,30 +59,60 @@ describe('Pokedex', () => {
   it('Teste se a Pokédex tem os botões de filtro.', async () => {
     renderWithRouter(<App />);
 
-    const all = await screen.findByRole('button', { name: /all/i });
-    expect(all).toBeInTheDocument();
+    const all = await screen.findAllByRole('button', { name: /all/i });
+    expect(all[0]).toBeInTheDocument();
+    expect(all).toHaveLength(1);
 
-    const Electric = await screen.findByRole('button', { name: /Electric/i });
-    expect(Electric).toBeInTheDocument();
+    const Electric = await screen.findAllByRole('button', { name: /Electric/i });
+    expect(Electric[0]).toBeInTheDocument();
+    expect(Electric).toHaveLength(1);
 
-    const Fire = await screen.findByRole('button', { name: /Fire/i });
-    expect(Fire).toBeInTheDocument();
+    const Fire = await screen.findAllByRole('button', { name: /Fire/i });
+    expect(Fire[0]).toBeInTheDocument();
+    expect(Fire).toHaveLength(1);
 
-    const Bug = await screen.findByRole('button', { name: /Bug/i });
-    expect(Bug).toBeInTheDocument();
+    const Bug = await screen.findAllByRole('button', { name: /Bug/i });
+    expect(Bug[0]).toBeInTheDocument();
+    expect(Bug).toHaveLength(1);
 
-    const Poison = await screen.findByRole('button', { name: /Poison/i });
-    expect(Poison).toBeInTheDocument();
+    const Poison = await screen.findAllByRole('button', { name: /Poison/i });
+    expect(Poison[0]).toBeInTheDocument();
+    expect(Poison).toHaveLength(1);
 
-    const Psychic = await screen.findByRole('button', { name: /Psychic/i });
-    expect(Psychic).toBeInTheDocument();
+    const Psychic = await screen.findAllByRole('button', { name: /Psychic/i });
+    expect(Psychic[0]).toBeInTheDocument();
+    expect(Psychic).toHaveLength(1);
 
-    const Normal = await screen.findByRole('button', { name: /Normal/i });
-    expect(Normal).toBeInTheDocument();
+    const normal = await screen.findAllByRole('button', { name: /Normal/i });
+    expect(normal[0]).toBeInTheDocument();
+    expect(normal).toHaveLength(1);
 
-    const Dragon = await screen.findByRole('button', { name: /Dragon/i });
-    expect(Dragon).toBeInTheDocument();
+    const dragonTestId = await screen.findAllByTestId('pokemon-type-button');
+    const dragon = await screen.findAllByRole('button', { name: /dragon/i });
+    expect(dragonTestId[6]).toBeInTheDocument();
+    expect(dragonTestId[6]).toHaveTextContent(/dragon/i);
+    expect(dragon).toHaveLength(1);
   });
+
+  it('A partir da seleção a Pokédex deve circular somente pelos pokémons daquele tipo',
+    async () => {
+      renderWithRouter(<App />);
+
+      const Fire = await screen.findByRole('button', { name: /Fire/i });
+      userEvent.click(Fire);
+
+      const charmander = await screen.findByText(/charmander/i);
+      expect(charmander).toBeInTheDocument();
+
+      const next = await screen.findByRole('button', { name: /próximo pokémon/i });
+      userEvent.click(next);
+
+      const rapidash = await screen.findByText(/rapidash/i);
+      expect(rapidash).toBeInTheDocument();
+
+      const all = await screen.findByRole('button', { name: /all/i });
+      expect(all).toBeInTheDocument();
+    });
 
   it('Teste se a Pokédex contém um botão para resetar o filtro', async () => {
     renderWithRouter(<App />);
@@ -90,12 +120,36 @@ describe('Pokedex', () => {
     const all = await screen.findByRole('button', { name: /all/i });
     expect(all).toBeInTheDocument();
   });
+
+  it('A Pokedéx deverá mostrar todos pokémons quando o botão All for clicado',
+    async () => {
+      renderWithRouter(<App />);
+
+      const btnNext = await screen.findByRole('button', { name: /próximo pokémon/i });
+      const filterAll = screen.getByRole('button', { name: /all/i });
+      expect(filterAll).toBeInTheDocument();
+
+      userEvent.click(filterAll);
+
+      const pikachu = await screen.findByText(/pikachu/i);
+      expect(pikachu).toBeInTheDocument();
+
+      userEvent.click(btnNext);
+
+      const charmander = await screen.findByText(/charmander/i);
+      expect(charmander).toBeInTheDocument();
+    });
+
+  it('Ao carregar a página, o filtro selecionado deverá ser All', async () => {
+    renderWithRouter(<App />);
+
+    const btnNext = await screen.findByRole('button', { name: /próximo pokémon/i });
+    const pikachu = await screen.findByText(/pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+
+    userEvent.click(btnNext);
+
+    const charmander = await screen.findByText(/charmander/i);
+    expect(charmander).toBeInTheDocument();
+  });
 });
-
-/*
-A Pokedéx deverá mostrar os pokémons normalmente (sem filtros) quando o botão All for clicado; (como checar se apenas mostra um pokemon por vez?)
-
-Ao carregar a página, o filtro selecionado deverá ser All; (Como checar se ele esta selecionado?)
-
-(o que os erros significam)
-*/
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('PokemonDetails', () => {
+  it('se as informações detalhadas do pokémon selecionado são mostradas na tela.',
+    async () => {
+      renderWithRouter(<App />);
+
+      const linkDetalhes = await screen.findByRole('link', { name: /More details/i });
+
+      userEvent.click(linkDetalhes);
+
+      const detailPageTitle = await screen
+        .findByRole('heading', { name: /pikachu details/i });
+      expect(detailPageTitle).toBeInTheDocument();
+      // Não deve existir o link de navegação para os detalhes do pokémon selecionado.
+      expect(linkDetalhes).not.toBeInTheDocument();
+
+      // A seção de detalhes deve conter um heading h2 com o texto Summary.
+      const summary = await screen.findByRole('heading', { level: 2, name: /summary/i });
+      expect(summary).toBeInTheDocument();
+
+      // A seção de detalhes deve conter um parágrafo com o resumo do pokémon específico sendo visualizado.
+      const paraAbout = await screen
+        .findByText(/This intelligent Pokémon roasts hard berries/i);
+      expect(paraAbout).toBeInTheDocument();
+    });
+
+  it('se existe na página uma seção com os mapas contendo as localizações do pokémon',
+    () => {
+
+    });
+});
